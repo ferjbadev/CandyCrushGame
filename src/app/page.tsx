2744cb8780349c02c281220702d7f4ab82973c95
Fernando Barrera <barrerafernando54@gmail.com>
2025-09-27 01:08:52 -0400

    upgrade page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,19 @@ const candyColors = ["red", "blue", "green", "yellow", "orange", "purple"];
 export default function Home() {
   const [board, setBoard] = useState<string[]>([]);
 
+  const checkForColumnOfThree = () => {
+    const newBoard = [...board];
+    for (let i = 0; i < width * width; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = board[i];
+
+      if (columnOfThree.every(index => board[index] === decidedColor)) {
+        columnOfThree.forEach(index => newBoard[index] = '');
+      }    
+    }
+    setBoard(newBoard);
+  };
+
   const createBoard = () => {
     const board: string[] = [];
     for (let i = 0; i < width * width; i++) {
@@ -22,13 +35,22 @@ export default function Home() {
     createBoard();
   }, []);
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setBoard([...board]);
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, [checkForColumnOfThree]);
+
   console.log(board);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-8">
-      <div className="w-[560px] h-[560px] flex flex-wrap gap-2">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-[560px] h-[560px] flex flex-wrap gap-2 ">
         {board.map((candy, index) => (
-          <div key={index} className=" w-12 h-12" style={{ backgroundColor: candy }}></div>
+          <div key={index} className=" w-14 h-14" style={{ backgroundColor: candy }}></div>
         ))}
       </div>
     </div>
